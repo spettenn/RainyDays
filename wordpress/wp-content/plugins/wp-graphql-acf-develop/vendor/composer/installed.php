34e32770b12b3c6e4da2fd3b1cb3b21a0d5034d1
spettenn <69841778+spettenn@users.noreply.github.com>
2021-04-23 14:00:05 +0200

    css

---
+++ wordpress/wp-content/plugins/wp-graphql-acf-develop/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'd0ca31f6391c80cd370d4da71aa185658ee3c5b9',
+    'name' => 'wp-graphql/wp-graphql-acf',
+  ),
+  'versions' => 
+  array (
+    'wp-graphql/wp-graphql-acf' => 
+    array (
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd0ca31f6391c80cd370d4da71aa185658ee3c5b9',
+    ),
+  ),
+);
